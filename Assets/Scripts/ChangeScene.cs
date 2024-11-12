c7ea22c3ecddfb51d31999356207a1cf2008d7a1
ryo <65832968+ryosafadi@users.noreply.github.com>
2024-11-11 23:40:15 -0800

    add instructions and play again

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,14 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChangeScene : MonoBehaviour
+{
+    [SerializeField] string sceneName;
+
+    public void SwitchScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
